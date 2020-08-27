b749e5f263d9c3dd1704c60ba760bead40a08c83
sunnystark <sunnystark19@gmail.com>
2020-08-27 18:43:34 +0530

    adding componet

---
+++ src/_component/home/home.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ProtoType from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+const styles = ({
+    root: {
+        flexGrow: 1,
+    }
+})
+
